bbd8430009df9ac9b8ba44da16ae8e773ee6a1df
Brandon <brandonpretelt93@gmail.com>
2016-08-31 04:27:56 -0600

    these ones

--- js/script.js
+++ js/script.js
@@ -7,31 +7,65 @@
 // This is a fun little project so I can practice my JavaScript.
 // It's also to keep me from being super lazy... haha
 
-var quotes = ["a warrior is as a warrior does", "here and now, breathe and relax", "the method is simple action", "a warrior acts and the fool reacts"];
+var quotes = ["a warrior is as a warrior does", "here and now, breathe and relax", "the method is simple action", "a warrior acts and the fool reacts", "difficult roads often lead to beautiful destinations", "if there is now way, create one", "if you avoid conflict to keep peace, you start a war inside yourself"];
+
+
+// var colors = ["blue", "teal", "purple", "darkred", "pink"];
 
 
 // this function uses the random method of the Math object
 // to randomize the indices of the array parameter that gets passed in
 
-function generateRand(a) {
+function randomize(a) {
     'use strict';
     return a[Math.floor(Math.random() * a.length)];
 }
 
 
 // this function just inserts the quote into the
-// output. 
+// output.
 
 function insertQuote() {
     'use strict';
-    var o = _(".quote-output");
-        o.innerHTML = "<q class=\"bgColor\">" + generateRand(quotes) + "</q>";
+    var o = _$(".quote-output");
+        o.innerHTML = "<q class=\"bgColor\">" + randomize(quotes) + "</q>";
+    // this.style.backgroundColor = randomize(colors);
 }
 
-// jQuery-like selector function. I will use an underscore instead
+// this function's purpose is to push a value
+// from the #ayo textarea into the quotes array
+
+function addYourOwn() {
+    'use strict';
+    var ayo = _$("#ayo").value;
+
+    if (ayo === "") {
+        var errSpn = document.createElement("span"), errMsg = document.createTextNode("Enter a value!");
+
+        var div = _$(".no-err");
+
+        div.classList.remove("no-err");
+        div.classList.add("error-container");
+
+        errSpn.appendChild(errMsg)
+        div.appendChild(errSpn);
+
+        return;
+    } else if (ayo !== "") {
+        var uQ = _$(".user-quote");
+        quotes.push(ayo);
+        uQ.innerHTML += "<q class=\"bgColor\">" + _$("#ayo").value  + "</q>";
+        _$("#ayo").value = "";
+
+
+    }
+
+}
+
+// jQuery-like selector function. I will use an underscore$ instead
 // of a $ symbol
 
-function _(selector) {
+function _$(selector) {
     'use strict';
     return document.querySelector(selector);
 }
@@ -39,7 +73,8 @@ function _(selector) {
 // checks to see if "addEventListener" method exists
 
 if (document.addEventListener) {
-    _("button").addEventListener("click", insertQuote, false);
+    _$("button").addEventListener("click", insertQuote, false);
+    _$("#ayoBtn").addEventListener("click", addYourOwn, false);
 } else {
-    _("button").onclick = insertQuote;
+    _$("button").onclick = insertQuote;
 }
